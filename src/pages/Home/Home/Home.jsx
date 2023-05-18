73cf1d5245035e980b422f5da31a439cbf3e552f
morshedalam39 <morshedalam4239@gmail.com>
2023-05-18 15:18:14 +0600

    MeetOuer Team

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Gallery from '../Gallery/Gallery';
 import Sponsor from '../Sponsor/Sponsor';
+import OurTeam from '../../OurTeam/OurTeam';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Gallery></Gallery>
             <Sponsor></Sponsor>
+            <OurTeam></OurTeam>
         </div>
     );
 };
